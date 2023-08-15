13893b64e1f731ecf4e78b1b4a8457edbbb8bb2f
athulraj10 <athulraj165@gmail.com>
2023-08-15 20:34:37 +0530

    Router added in app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          
           <Route path="/" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/home" element={<Home/>}/>
